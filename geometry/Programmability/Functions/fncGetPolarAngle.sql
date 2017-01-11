0c33546f481aa4c2095f53603855db889260bc75
NightkingCH <thomas.erni@nk-s.ch>
2017-01-11 11:30:02 +0100

    Update and rename fncGetPolarAngle to fncGetPolarAngle.sql

--- geometry/Programmability/Functions/fncGetPolarAngle.sql
+++ geometry/Programmability/Functions/fncGetPolarAngle.sql
@@ -10,16 +10,13 @@ AS
 BEGIN
 	DECLARE @RADIAN DECIMAL(38,24) = 57.295779513082;
 	DECLARE @degrees DECIMAL(38,24) = 360.000000000000;
-	DECLARE 
-			@deltaX DECIMAL(38,24)
-			, @deltaY DECIMAL(38,24)
-			, @angle DECIMAL(38,24)
-		;
+	DECLARE @deltaX DECIMAL(38,24)
+		, @deltaY DECIMAL(38,24)
+		, @angle DECIMAL(38,24);
 
-	 (SELECT @deltaX = @comparePointX - @anchorPointX);
-	 (SELECT @deltaY = @comparePointY - @anchorPointY);
+	(SELECT @deltaX = @comparePointX - @anchorPointX);
+	(SELECT @deltaY = @comparePointY - @anchorPointY);
         
-
 	IF @deltaX = 0 AND @deltaY = 0
 		BEGIN
 			RETURN 0;
